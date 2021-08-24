b804fbebc50b6bbbbe64676337d23b84356205ec
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-08-24 22:13:29 +0200

    Let's not break the script if the exitcode is unkown

--- worldserver-exitcode-script.bat
+++ worldserver-exitcode-script.bat
@@ -22,12 +22,12 @@ if %errorlevel% == 2 (
 )
 
 :: For a custom shutdown code "15"
-if %errorlevel% == 15 (
-    echo Shutdown Code is %errorlevel%
-    echo Press any key to restart WorldServer.
-    pause
-    goto start
-)
+:: if %errorlevel% == 15 (
+::     echo Shutdown Code is %errorlevel%
+::     echo Press any key to restart WorldServer.
+::     pause
+::     goto start
+:: )
 
 :: For ERROR_EXIT_CODE
 if %errorlevel% == 1 (
@@ -36,4 +36,20 @@ if %errorlevel% == 1 (
     echo Press any key to restart WorldServer, press CTRL + C to exit.
     pause
     goto start
+)
+
+:: As batch doesn't have logical operators...
+:: This is what we have to do to not break the script if an
+:: unknown errorlevel is provided.
+if NOT %errorlevel% == 0 (
+    if NOT %errorlevel% == 1 (
+        if NOT %errorlevel% == 2 (
+            echo Shutdown Code is unknown.
+            echo Shutdown Code is %errorlevel%
+            echo Maybe this was a misstake? WorldServer will restart in 60s
+            echo Press CTRL + C to cancel.
+            timeout /t 60
+            goto start
+        )
+    )
 )
